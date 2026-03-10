d6ea905ad0d3ea5b554f48a1997597c124e09e62
Jungsoo520 <c28david.park@afacademy.af.edu>
2026-03-10 15:24:22 -0600

    WORKING

---
+++ src/sevenseg_decoder_tb.vhd
@@ -0,0 +1,43 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity sevenSegDecoder_tb is
+end sevenSegDecoder_tb;
+
+architecture tb of sevenSegDecoder_tb is
+
+  signal i_S : std_logic_vector(3 downto 0);
+  signal o_S : std_logic_vector(6 downto 0);
+
+begin
+
+  DUT: entity work.sevenSegDecoder
+    port map(
+      i_S => i_S,
+      o_S => o_S
+    );
+
+  process
+  begin
+    i_S <= "0000"; wait for 10 ns;
+    i_S <= "0001"; wait for 10 ns;
+    i_S <= "0010"; wait for 10 ns;
+    i_S <= "0011"; wait for 10 ns;
+    i_S <= "0100"; wait for 10 ns;
+    i_S <= "0101"; wait for 10 ns;
+    i_S <= "0110"; wait for 10 ns;
+    i_S <= "0111"; wait for 10 ns;
+    i_S <= "1000"; wait for 10 ns;
+    i_S <= "1001"; wait for 10 ns;
+    i_S <= "1010"; wait for 10 ns;
+    i_S <= "1011"; wait for 10 ns;
+    i_S <= "1100"; wait for 10 ns;
+    i_S <= "1101"; wait for 10 ns;
+    i_S <= "1110"; wait for 10 ns;
+    i_S <= "1111"; wait for 10 ns;
+
+    wait;
+  end process;
+
+end tb;
